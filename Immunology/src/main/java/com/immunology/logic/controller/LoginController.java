df94a56abdbd332d29fffd689bdc761a6a919107
Chemodurov Volodymyr <chemodurov92@gmail.com>
2014-04-14 23:33:44 +0300

    Add md5 password encryption.

--- Immunology/src/main/java/com/immunology/logic/controller/LoginController.java
+++ Immunology/src/main/java/com/immunology/logic/controller/LoginController.java
@@ -1,6 +1,5 @@
 package com.immunology.logic.controller;
 
-import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,16 +8,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/")
 public class LoginController {
 
-	private static final Logger LOG = Logger.getLogger(LoginController.class);
-	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login() {
 		return "login";
 	}
 	
-	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String onLogin() {
-		LOG.info("login");
-		return "index";
-	}
 }

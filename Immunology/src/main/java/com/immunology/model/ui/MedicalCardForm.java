2b4f5376e00b394585f6cadcdd63352682ec67ef
Chemodurov Volodymyr <chemodurov92@gmail.com>
2014-05-31 23:53:09 +0300

    Added stable panels elements persistent.

--- Immunology/src/main/java/com/immunology/model/ui/MedicalCardForm.java
+++ Immunology/src/main/java/com/immunology/model/ui/MedicalCardForm.java
@@ -2,18 +2,24 @@ package com.immunology.model.ui;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
+import org.codehaus.jackson.annotate.JsonTypeInfo;
+
+@JsonTypeInfo(use=JsonTypeInfo.Id.NAME, include=JsonTypeInfo.As.PROPERTY, property="objectType")
 @Entity
 @Table(name = "medical_forms")
 public class MedicalCardForm extends Form {
 
 	@Temporal(TemporalType.DATE)
+	@Column(name="creation_date")
 	private Date creationDate;
-	private String additionInfo;
+	@Column(name="additional_info")
+	private String additionalInfo;
 	
 	public Date getCreationDate() {
 		return creationDate;
@@ -22,10 +28,10 @@ public class MedicalCardForm extends Form {
 		this.creationDate = creationDate;
 	}
 	public String getAdditionInfo() {
-		return additionInfo;
+		return additionalInfo;
 	}
 	public void setAdditionInfo(String additionInfo) {
-		this.additionInfo = additionInfo;
+		this.additionalInfo = additionInfo;
 	}
 	
 }

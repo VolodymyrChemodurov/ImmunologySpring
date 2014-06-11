ac1c6c74869ea0be69c1fa6718c03b98d7bdda1b
Chemodurov Volodymyr <chemodurov92@gmail.com>
2014-06-11 23:24:38 +0300

    Solved encoding problem.

--- Immunology/src/main/changelog/test_data.sql
+++ Immunology/src/main/changelog/test_data.sql
@@ -1,9 +1,9 @@
 INSERT INTO users VALUES(1, 'John', 'Doy', 'user', 'Volodymyrovuch', '$2a$10$EzVF3davuFbb0.KmKJa4B.xZA.ax7QASX97gcnuq116DZ98UyQCn.');
 INSERT INTO user_roles VALUES(1, 1);
 
-INSERT INTO patient values(1, 'Lviv', 'Ukraine', '11-11-1992', 'Andrii', '231', 'Testovui', 'Volodymyrovych', 'Lvivska', 'male', 'Djerelna', 1);
+INSERT INTO patient values(1, 'Lviv', 'Ukraine', '11/11/1992', 'Andrii', '231', 'Testovui', 'Volodymyrovych', 'Lvivska', 'male', 'Djerelna', 1);
 INSERT INTO form VALUES(1, 'test form', 1);
-INSERT INTO medical_forms VALUES('additional information', '11-21-2001', 1);
+INSERT INTO medical_forms VALUES('additional information', '11/11/2001', 1);
 INSERT INTO element(element_id, checked, name, place) VALUES(1, false, 'Genetic (congenital) predisposition to allergies', 1);
 INSERT INTO panels VALUES(1,1);
 INSERT INTO element VALUES(2, false, 'Patients with allergic relatives', 1, 1);
@@ -22,20 +22,20 @@ INSERT INTO text_boxes VALUES('', 7);
 INSERT INTO element(element_id, checked, name, place) VALUES(8, false, 'Features a history of childhood', 2);
 INSERT INTO panels VALUES(8,1);
 INSERT INTO element VALUES(9, false, 'Breastfeeding', 1, 8);
-INSERT INTO dropdowns VALUES('', 9);
+INSERT INTO dropdowns VALUES(0, '', 9);
 INSERT INTO dropdown_values VALUES(9, 0, 'Less than 3 months');
 INSERT INTO dropdown_values VALUES(9, 0, 'Less than 6 months');
 INSERT INTO dropdown_values VALUES(9, 0, 'Less than 12 months');
 
 INSERT INTO element VALUES(10, false, 'Having allergies in childhood', 2, 8);
-INSERT INTO dropdowns VALUES('', 10);
+INSERT INTO dropdowns VALUES(0, '', 10);
 INSERT INTO dropdown_values VALUES(10, 0, 'Yes');
 INSERT INTO dropdown_values VALUES(10, 0, 'No');
 
 INSERT INTO element(element_id, checked, name, place) VALUES(11, false, 'Accommodation', 3);
 INSERT INTO panels VALUES(11,1);
 INSERT INTO element VALUES(12, false, 'The severity of living conditions', 1, 11);
-INSERT INTO dropdowns VALUES('', 12);
+INSERT INTO dropdowns VALUES(0, '', 12);
 INSERT INTO dropdown_values VALUES(12, 0, '0');
 INSERT INTO dropdown_values VALUES(12, 0, '1');
 INSERT INTO dropdown_values VALUES(12, 0, '2');

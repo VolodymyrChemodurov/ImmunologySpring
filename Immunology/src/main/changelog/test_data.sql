8ca8dc32ae5fac711fb59db273ca95c3483ecbde
Chemodurov Volodymyr <chemodurov92@gmail.com>
2014-07-02 22:47:04 +0300

    Fixed sql test data scripts.

--- Immunology/src/main/changelog/test_data.sql
+++ Immunology/src/main/changelog/test_data.sql
@@ -1,9 +1,10 @@
 INSERT INTO users VALUES(1, 'John', 'Doy', 'user', 'Volodymyrovuch', '$2a$10$EzVF3davuFbb0.KmKJa4B.xZA.ax7QASX97gcnuq116DZ98UyQCn.');
 INSERT INTO user_roles VALUES(1, 1);
 
-INSERT INTO patient values(1, 'Lviv', 'Ukraine', '11/11/1992', 'Andrii', '231', 'Testovui', 'Volodymyrovych', 'Lvivska', 'male', 'Djerelna', 1);
-INSERT INTO form VALUES(1, 'test form', 1);
-INSERT INTO medical_forms VALUES('additional information', '11/11/2001', 1);
+INSERT INTO patient values(1, 'Lviv', 'Ukraine', '11/11/1992', 'Andrii', '231', 'Testovui', 'Volodymyrovych', 'Lvivska', 'male', 'Djerelna');
+INSERT INTO user_patient VALUES(1, 1);
+INSERT INTO form VALUES(1, 'test form');
+INSERT INTO medical_card_forms VALUES('additional information', '11/11/2001', 1, 1);
 INSERT INTO element(element_id, checked, name, place) VALUES(1, false, 'Genetic (congenital) predisposition to allergies', 1);
 INSERT INTO panels VALUES(1,1);
 INSERT INTO element VALUES(2, false, 'Patients with allergic relatives', 1, 1);
--- Immunology/src/main/changelog/test_data_ua.sql
+++ Immunology/src/main/changelog/test_data_ua.sql
@@ -3,8 +3,9 @@ INSERT INTO user_roles VALUES(1, 1);
 
 INSERT INTO patient values(1, 'Lviv', 'Ukraine', '11/11/1992', 'Andrii', '231', 'Testovui', 'Volodymyrovych', 'Lvivska', 'male', 'Djerelna');
 INSERT INTO user_patient VALUES(1, 1);
-INSERT INTO form VALUES(1, 'test form', 1);
-INSERT INTO medical_forms VALUES('Додаткова інформація', '11/11/2001', 1);
+
+INSERT INTO form VALUES(1, 'test form');
+INSERT INTO medical_card_forms VALUES('Додаткова інформація', '11/11/2001', 1, 1);
 INSERT INTO element(element_id, checked, name, place) VALUES(1, false, 'Генетична (вроджена) схильність до алергії', 1);
 INSERT INTO panels VALUES(1,1);
 INSERT INTO element VALUES(2, false, 'Хворі на алергію родичі', 1, 1);
